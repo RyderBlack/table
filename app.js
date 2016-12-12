c6fd1e4f1177fed46e5d4d79f1d09556d9426220
Royalty <royalty@w117.wguest.ec-m.fr>
2016-12-12 10:11:04 +0100

    update 1.02

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const ready = function() {
+/*const ready = function() {
 
-    const famousPeople = {
-        lastname: "Jackson",firstname : "Mickael",
-        lastname: "Jackson",firstname : "Janet",
-        lastname: "Jackson",firstname : "LaToya",
-        lastname: "Jackson",firstname : "les autres"
-    };
+    const famousPeople = [
+        {lastname: "Jackson",firstname : "Mickael"},
+        {lastname: "Jackson",firstname : "Janet"},
+        {lastname: "Jackson",firstname : "LaToya"},
+        {lastname: "Jackson",firstname : "les autres"}
+    ];
 
     const tbody = document.querySelector('tbody');
+
+let trContainer =
 
     for (let i = 0; i < famousPeople.length; i++) {
 
         let tr = document.createElement("tr");
-        tr.innerHTML = `<td>${famousPeople[i].firstname}</td>`;
+        tr.innerHTML = `<td>${famousPeople[i].lastname}</td><td>${famousPeople[i].firstname}</td>`;
         tbody.appendChild(tr);
 
-        /*
+
+
         let tdfirst = document.createElement('td');
         let tdlast = document.createElement('td');
         let firstname = document.createTextNode(famousPeople[i].firstname);
@@ -23,8 +26,34 @@ const ready = function() {
         td.appendChild(firstname);
         td.appendChild(lastname);
         tr.appendChild(td);
-        */
+
     }
+
+}
+
+document.addEventListener("DOMContentLoaded", ready);
+*/
+
+const ready = function() {
+  // Loading
+  const famousPeople = [
+    {lastname: "Jackson",firstname : "Mickael", age: 50, height: 5.9},
+    {lastname: "Jackson",firstname : "Janet", age: 50,  height: 5.4},
+    {lastname: "Jackson",firstname : "LaToya", age: 60,  height: 5.6},
+    {lastname: "Jackson",firstname : "les autres", age: 150,  height: 5.9}
+  ];
+  //
+  const tbody = document.querySelector("tbody");
+
+  let trContainer = document.createDocumentFragment();
+
+  for(let i = 0; i < famousPeople.length; i++) {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `<td>${famousPeople[i].lastname}</td><td>${famousPeople[i].firstname}</td><td>${famousPeople[i].age}</td><td>${famousPeople[i].height}</td>`;
+      trContainer.appendChild(tr);
+  }
+
+  tbody.appendChild(trContainer);
 
 }
 
